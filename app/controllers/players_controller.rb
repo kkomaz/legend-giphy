75b740aa39566a6784f5c326ad1b32d7457b5ec3
kkomaz <leeajl006@gmail.com>
2015-04-23 23:15:47 -0400

    moved api call from controller to models

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,7 +6,5 @@ class PlayersController < ApplicationController
   def show
     @player = Player.find(params[:id])
     @player_stats = Kaminari.paginate_array(@player.stats).page(params[:page]).per(4)
-    @client = TwitterApi.new
-    @player_twitter = @client.get_twitter_feed(@player.twitter_name).take(3)
   end
 end
