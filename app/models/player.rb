acf346bed8e10b00549fb6bbdb107e6906d2755d
kkomaz <leeajl006@gmail.com>
2015-04-29 23:25:40 -0400

    refactor

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,13 +2,6 @@ class Player < ActiveRecord::Base
   has_many :stats
   has_many :characters, :through => :stats
 
-  def pull_tweets
-    # tweets = []
-    # TwitterApi.new.get_twitter_feed(self).each do |tweet|
-    #   html = client.oembed(tweet.id).html
-    #   tweets <<
-  end
-
   def pull_videos
     YoutubeApi.new.get_youtube_vid(self.name)
   end
